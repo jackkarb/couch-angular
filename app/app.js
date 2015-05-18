fc89c60c9658de6f75771b884e40a692cff259d3
jackkarb <jkarbiner@gmail.com>
2015-05-18 09:03:24 -0700

    wow cool

--- app/app.js
+++ app/app.js
@@ -6,7 +6,43 @@ angular.module('myApp', [
   'myApp.view1',
   'myApp.view2',
   'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+])
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'main.html',
+            controller: 'mainCtrl'
+        });
+        $routeProvider.when('/a2000', {
+            templateUrl: 'asap/a2000.html',
+            controller: 'a2000Ctrl'
+        });
+      $routeProvider.otherwise({redirectTo: '/view1'});
+}])
+    .controller('mainCtrl', ['$scope', '$http',function($scope, $http) {
+        $http.get('http://localhost:5984')
+            .success(function (data) {
+                $scope.data = data;
+
+            });
+        $http.get('http://localhost:5984/jacks_db')
+            .success(function (dat2) {
+                $scope.dat2 = dat2;
+                $scope.props = Object.keys(dat2);
+
+            });
+    }])
+    .controller('a2000Ctrl', ['$scope', '$http',function($scope, $http) {
+        $http.get('http://localhost:5984/jacks_db/2c6749cf4d7fe47db91873337503368e')
+            .success(function (data) {
+                $scope.data = data.a2000;
+
+            });
+        /*
+        $http.get('http://localhost:5984/jacks_db')
+            .success(function (dat2) {
+                $scope.dat2 = dat2;
+                $scope.props = Object.keys(dat2);
+
+            });
+        */
+    }]);
