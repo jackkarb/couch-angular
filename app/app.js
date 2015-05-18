a43642b9f5b26aec2cf029c0bdd28ff55c9eb399
jackkarb <jkarbiner@gmail.com>
2015-05-18 18:33:40 -0400

    commit

--- app/app.js
+++ app/app.js
@@ -1,10 +1,8 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
+// Declare app level module which depends on views, and components jack
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
   'myApp.version'
 ])
     .config(['$routeProvider', function($routeProvider) {
