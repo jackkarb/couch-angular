aa9fe0d61d76f3ceab7cbc94cde3aa92ad8acd97
jackkarb <jkarbiner@gmail.com>
2015-05-19 12:07:00 -0700

    commit

--- app/app.js
+++ app/app.js
@@ -22,41 +22,23 @@ angular.module('myApp', [
         var Db = $resource('http://localhost:5984');
         var db = Db.get({}, function(){
                 $scope.data = db;
-                console.log('Resource ' + angular.toJson(db));}
-        );
+                console.log('Resource ' + angular.toJson(db));
+        });
 
         var jkDb = $resource('http://localhost:5984/:db');
         var jk = jkDb.get({db:'jacks_db'}, function(){
                 $scope.props = Object.keys(jk);
                 $scope.dat2 = jk;
-                console.log('Resource' + angular.toJson(jk));}
-        );
-       /*
-        $http.get('http://localhost:5984')
-            .success(function (data) {
-                $scope.data = data;
+                console.log('Resource' + angular.toJson(jk));
+        });
 
-            });
-        $http.get('http://localhost:5984/jacks_db')
-            .success(function (dat2) {
-                $scope.dat2 = dat2;
-                $scope.props = Object.keys(dat2);
-
-            });
-         */
     }])
-    .controller('a2000Ctrl', ['$scope', '$http',function($scope, $http) {
-        $http.get('http://localhost:5984/jacks_db/2c6749cf4d7fe47db91873337503368e')
-            .success(function (data) {
-                $scope.data = data.a2000;
+    .controller('a2000Ctrl', ['$scope', '$http', '$resource', function($scope, $http, $resource) {
 
-            });
-        /*
-        $http.get('http://localhost:5984/jacks_db')
-            .success(function (dat2) {
-                $scope.dat2 = dat2;
-                $scope.props = Object.keys(dat2);
+        var A2k = $resource('http://localhost:5984/:db/:id');
+        var a2k = A2k.get({db: 'jacks_db', id: '2c6749cf4d7fe47db91873337503368e'}, function() {
+            $scope.data = a2k.a2000;
+            console.log('a2k' + angular.toJson(a2k));
+        });
 
-            });
-        */
     }]);
