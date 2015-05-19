2cc509a64a9420b1c276a8a0cf9a268f616a57ad
jackkarb <jkarbiner@gmail.com>
2015-05-19 08:14:24 -0700

    commit

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@
 // Declare app level module which depends on views, and components jack
 angular.module('myApp', [
   'ngRoute',
+  'ngResource',
   'myApp.version'
 ])
     .config(['$routeProvider', function($routeProvider) {
@@ -16,7 +17,21 @@ angular.module('myApp', [
         });
       $routeProvider.otherwise({redirectTo: '/view1'});
 }])
-    .controller('mainCtrl', ['$scope', '$http',function($scope, $http) {
+    .controller('mainCtrl', ['$scope', '$http', '$resource', function($scope, $http, $resource) {
+
+        var Db = $resource('http://localhost:5984');
+        var db = Db.get({}, function(){
+                $scope.data = db;
+                console.log('Resource ' + angular.toJson(db));}
+        );
+
+        var jkDb = $resource('http://localhost:5984/:db');
+        var jk = jkDb.get({db:'jacks_db'}, function(){
+                $scope.props = Object.keys(jk);
+                $scope.dat2 = jk;
+                console.log('Resource' + angular.toJson(jk));}
+        );
+       /*
         $http.get('http://localhost:5984')
             .success(function (data) {
                 $scope.data = data;
@@ -28,6 +43,7 @@ angular.module('myApp', [
                 $scope.props = Object.keys(dat2);
 
             });
+         */
     }])
     .controller('a2000Ctrl', ['$scope', '$http',function($scope, $http) {
         $http.get('http://localhost:5984/jacks_db/2c6749cf4d7fe47db91873337503368e')
