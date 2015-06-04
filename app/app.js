effeda0021e50b204b014fa78aad55575a1bd3c2
jacob karbiner <jkarbiner@gmail.com>
2015-06-04 09:41:57 -0400

    upload works

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@
 angular.module('myApp', [
   'ngRoute',
   'ngResource',
-  'angularFileUpload',
+  'ngFileUpload',
   'myApp.version'
 ])
     .config(['$routeProvider', function($routeProvider) {
@@ -48,56 +48,37 @@ angular.module('myApp', [
 
 
     }])
-    .controller('togoCtrl', ['$scope', '$http', '$resource','FileUploader', function($scope, $http, $resource, FileUploader) {
-        var uploader = $scope.uploader = new FileUploader({
-                    url: 'http://localhost:5984/jacks_db'
-                 });
+    .controller('togoCtrl', ['$scope', '$http', '$resource','Upload', function($scope, $http, $resource, Upload) {
 
-        uploader.filters.push({
-                         name: 'customFilter',
-                         fn: function(item /*{File|FileLikeObject}*/, options) {
-                            return this.queue.length < 10;
-                        }
-                });
+        $scope.$watch('files', function () {
+            $scope.upload($scope.files);
+        });
+
+        $scope.upload = function (files) {
+            if (files && files.length) {
+                for (var i = 0; i < files.length; i++) {
+                    var file = files[i];
+                    Upload.http({
+                        method:'PUT',
+                        //url: 'upload/'+file.name,
+                        url: 'http://localhost:5984/jacks_db/aa868a443eebf37733456bec8600140f/attachment?rev=3-3e38108a70d7ab1f2e5cd376b70ad04f',
+                        headers: {'Content-Type': file.type},
+                        data: file
+                    })
+                    .progress(function (evt) {
+                        var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+                        console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
+                    }).success(function (data, status, headers, config) {
+                            $scope.file = file;
+                        console.log('file ' + config.file.name + 'uploaded. Response: ' + data);
+                    });
+                }
+            }
+        };
 
 
-        // CALLBACKS
 
-        uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/, filter, options) {
-                console.info('onWhenAddingFileFailed', item, filter, options);
-            };
-        uploader.onAfterAddingFile = function(fileItem) {
-                console.info('onAfterAddingFile', fileItem);
-             };
-        uploader.onAfterAddingAll = function(addedFileItems) {
-                console.info('onAfterAddingAll', addedFileItems);
-             };
-        uploader.onBeforeUploadItem = function(item) {
-                console.info('onBeforeUploadItem', item);
-             };
-        uploader.onProgressItem = function(fileItem, progress) {
-                console.info('onProgressItem', fileItem, progress);
-            };
-        uploader.onProgressAll = function(progress) {
-                console.info('onProgressAll', progress);
-            };
-        uploader.onSuccessItem = function(fileItem, response, status, headers) {
-                console.info('onSuccessItem', fileItem, response, status, headers);
-            };
-        uploader.onErrorItem = function(fileItem, response, status, headers) {
-                console.info('onErrorItem', fileItem, response, status, headers);
-             };
-        uploader.onCancelItem = function(fileItem, response, status, headers) {
-                 console.info('onCancelItem', fileItem, response, status, headers);
-             };
-         uploader.onCompleteItem = function(fileItem, response, status, headers) {
-                 console.info('onCompleteItem', fileItem, response, status, headers);
-             };
-        uploader.onCompleteAll = function() {
-                 console.info('onCompleteAll');
-             };
 
-        console.info('uploader', uploader);
 /*
 
 $scope.upload = function (files) {
