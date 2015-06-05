7f4f6091363dcb2175a6ca8c37120e1a4bdf122b
jacob karbiner <jkarbiner@gmail.com>
2015-06-05 13:46:46 -0400

    upload works

--- app/app.js
+++ app/app.js
@@ -39,16 +39,24 @@ angular.module('myApp', [
 
     }])
     .controller('a2000Ctrl', ['$scope', '$http', '$resource', function($scope, $http, $resource) {
-
-        var A2k = $resource('http://localhost:5984/:db/:id');
-        var a2k = A2k.get({db: 'jacks_db', id: '2c6749cf4d7fe47db91873337503368e'}, function() {
-            $scope.data = a2k.a2000;
-            console.log('a2k' + angular.toJson(a2k));
-        });
+      }])
 
 
-    }])
     .controller('togoCtrl', ['$scope', '$http', '$resource','Upload', function($scope, $http, $resource, Upload) {
+
+        $scope.save = function() {
+            var UUID = $resource('http://localhost:5984/_uuids');
+            var uuid = UUID.get({}, function() {
+                $scope.$uid = uuid.uuids[0];
+                //console.log($scope.id + angular.toJson($scope.data));
+            });
+
+            var Place = $resource('http://localhost:5984/:db/:uid', null, {'create': {method: 'PUT'}});
+            console.log($scope.$uid );
+            var place = Place.create({db:'jacks_db', uid: $scope.$uid}, $scope.data);
+            console.log(place.rev );
+        };
+
 
         $scope.$watch('files', function () {
             $scope.upload($scope.files);
@@ -76,11 +84,7 @@ angular.module('myApp', [
             }
         };
 
-
-
-
 /*
-
 $scope.upload = function (files) {
             if (files && files.length) {
                 for (var i = 0; i < files.length; i++) {
@@ -98,7 +102,6 @@ $scope.upload = function (files) {
                 }
             }
         };
-
         /*
                 var A2k = $resource('http://localhost:5984/:db/:id');
                 var a2k = A2k.get({db: 'jacks_db', id: '2c6749cf4d7fe47db91873337503368e'}, function() {
