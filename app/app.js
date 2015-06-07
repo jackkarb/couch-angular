1582838e60f324ed731b9bd4dfffe09fa350e1d9
jackkarb <jkarbiner@gmail.com>
2015-06-07 18:32:59 -0400

    commit

--- app/app.js
+++ app/app.js
@@ -49,9 +49,14 @@ angular.module('myApp', [
             UUID.get({}, function(uuid) {
                 ///var uuid = UUID.get({}, function() {
                 $scope.uid = uuid.uuids[0];
-               //console.log($scope.uid + angular.toJson($scope.data));
-               // var Place = $resource('http://localhost:5984/:db/:uid', null, {'create': {method: 'PUT'}});
-               // var place = Place.create({db:'jacks_db', uid: $scope.uid}, $scope.data);
+               /*
+                var Place = $resource('http://localhost:5984/:db/:uid', null, {'create': {method: 'PUT'}});
+                var place = Place.create({db:'jacks_db', uid: $scope.uid}, $scope.data)
+                    .success(function (data, status, headers, config) {
+                        $scope.rev = data.rev;
+                        console.log(data);
+                    });
+                */
                $http.put('http://localhost:5984/jacks_db/'+$scope.uid, $scope.data )
                     .success(function (data, status, headers, config) {
                         $scope.rev = data.rev;
@@ -95,29 +100,5 @@ angular.module('myApp', [
             }
         };
 
-/*
-$scope.upload = function (files) {
-            if (files && files.length) {
-                for (var i = 0; i < files.length; i++) {
-                    var file = files[i];
-                    Upload.upload({
-                        url: 'upload/url',
-                        fields: {'username': $scope.username},
-                        file: file
-                    }).progress(function (evt) {
-                        var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-                        console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
-                    }).success(function (data, status, headers, config) {
-                        console.log('file ' + config.file.name + 'uploaded. Response: ' + data);
-                    });
-                }
-            }
-        };
-        /*
-                var A2k = $resource('http://localhost:5984/:db/:id');
-                var a2k = A2k.get({db: 'jacks_db', id: '2c6749cf4d7fe47db91873337503368e'}, function() {
-                    $scope.data = a2k.a2000;
-                    console.log('a2k' + angular.toJson(a2k));
-                });
-         */
+
     }]);
